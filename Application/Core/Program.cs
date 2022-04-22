af84edaddf9bffb6ed30988989e28eb81c44b421
Ivan Fedorchenko <65971171+ivan70f@users.noreply.github.com>
2022-04-22 20:19:33 +0300

    Init

---
+++ Application/Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Core
+{
+    internal static class Program
+    {
+        public static void Main(string[] _args)
+        {
+            Server.Start(50, 26950);
+        }
+    }
+}
